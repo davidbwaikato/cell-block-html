e77d9fb77a21f3fadd0c9df54c7c893ac5ff2941
David Bainbridge <davidb@waikato.ac.nz>
2018-07-18 14:23:28 +1200

    Further refinements

--- SETUP.bash
+++ SETUP.bash
@@ -1,7 +1,25 @@
 
 export CELL_BLOCK_HTML_HOME=`pwd`
 
-if [ -d /opt/nodejs/bin ] ; then
-  export PATH=/opt/nodejs/bin/:$PATH
-  echo "Added NodeJS into PATH"
+echo
+echo "Checking for npm"
+which npm 2>/dev/null
+
+if [ $? != 0 ] ; then
+  # Failed to find
+  if [ -d "/opt/nodejs/bin" ] ; then
+    export PATH=/opt/nodejs/bin/:$PATH
+    echo "Added NodeJS into PATH"
+  else
+    echo "Failed to find" 1>&2
+    return
+  fi
+else
+  echo
+  echo "Suggested minimum version of npm for Cell Block HTML: 5.6.0"
+  npm_version=`npm -v --quiet`
+  echo "Version of detected npm: $npm_version"
+  echo
 fi
+
+
